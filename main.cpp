e4b660ab893c12a26307f61a4aed82921d2390de
Andrey <anevshvets@edu.hse.ru>
2024-03-08 00:20:54 +0300

    Fixed issue is SendBind, added bmp parser (with strong support of Sokolova)

--- main.cpp
+++ main.cpp
@@ -2,32 +2,58 @@
 #include "Display.h"
 #include "constants.h"
 #include "Keyboard.h"
+#include "bitmap_image.hpp"
 //#include <ncurses.h>
 #include <curses.h>
 #include <string>
 #include <thread>
+#include <map>
 using std::vector;
+
+
 
 int main() {
     Display display;
-    Keyboard::getKeyPressed(display);
-    WindowEvent event(WindowEvent::INFO, "ABA");
+    WindowEvent event(WindowEvent::INFO, "ABAB");
     display.SendEvent(event);
-    WindowEvent event2(WindowEvent::INFO, std::to_string(5));
-    display.SendEvent(event2);
-    WindowEvent event3(WindowEvent::INFO, "CABA");
-    display.SendEvent(event3);
+    Bind bind(97, true, "ABACABA");
+    display.SendBind(bind);
+    std::cin.get();
+    ColorManager manager;
+    bitmap_image image("bmp_24.bmp");
+    vector<vector<unsigned>> sprite(50, vector<unsigned>(100));
+    for (std::size_t y = 0; y < 50; ++y)
+    {
+        for (std::size_t x = 0; x < 100; ++x)
+        {
+            rgb_t colour;
 
+            image.get_pixel(x, y, colour);
+
+            unsigned clr = manager.CreateColorPair(-1, ColorManager::getColor(colour.red, colour.green, colour.blue));
+
+            sprite[y][x] = ' ' | clr;
+
+        }
+    }
+    display.DrawSprite(sprite, 0, 0);
     while(true) {
         int key = Keyboard::getKeyPressed(display);
         if(key == -1) continue;
-        if(key == 27) { //esc
+        if(key == 32) {
             break;
         }
-        WindowEvent event5(WindowEvent::INFO, std::to_string(key));
-        display.SendEvent(event5);
+        if(key == 97) {
+            Bind bind2(97, false);
+            display.SendBind(bind2);
+        }
+        else{
+            WindowEvent event2(WindowEvent::INFO, std::to_string(key));
+            display.SendEvent(event2);
+            Bind bind2(key, true);
+            display.SendBind(bind2);
+        }
     }
-
     std::cin.get();
     return 0;
 }
