7a59e12b75280079d4a0df224a0114dc228c4362
anton <anton.a.kozhevnikov@gmail.com>
2024-03-17 12:48:24 +0300

    added sprite for ability, fixed bugs

--- GameData/Ability.h
+++ GameData/Ability.h
@@ -1,14 +1,18 @@
 #pragma once
 
 #include <string>
+#include <vector>
 
 class Ability {
 public:
     Ability() = default;
-    Ability(std:: string name, std::string hint, bool team, bool multitarget, double damage, double heal, bool healStatus, int burn, int stun);
+    Ability(std:: string name, std::string hint, std::vector<std::vector<unsigned>> sprite, bool team, bool multitarget, double damage, double heal, bool healStatus, int burn, int stun);
     Ability(const Ability &other);
     Ability& operator=(const Ability &other);
 
+    std::string get_name() const;
+    std::string get_hint() const;
+    std::vector<std::vector<unsigned int>> get_sprite() const;
     bool get_team() const;
     bool get_multitarget() const;
     double get_damage() const;
@@ -18,6 +22,7 @@ public:
 private:
     std::string name;
     std::string hint;
+    std::vector<std::vector<unsigned int>> sprite;
     bool team; //true if apply to self, false if apply to enemy
     bool multitarget; //true if apply to all enemies, false if apply to one enemy
     double damage; //proportion of unit's dmg, 0 if don't deal damage
--- GameData/BattleSamples.h
+++ GameData/BattleSamples.h
@@ -3,13 +3,13 @@
 
 namespace SampleAbilities {
     // order: name, hint, team, multitarget, damage, heal, healStatus, burn, stun
-    static Ability basicAttack = Ability("Basic Attack", "Deal damage to one enemy", false, false, 1, 0, 0, 0, 0);
-    static Ability shieldBash = Ability("Shield Bash", "Stun one enemy for 2 moves", false, false, 0.5, 0, 0, 0, 2);
-    static Ability roar = Ability("Roar", "Heal one ally", true, false, 0, 0, 1, 0, 0);
-    static Ability fireStorm = Ability("Fire Storm", "Deal damage to all enemies", false, true, 0.3, 0, 0, 0, 0);
-    static Ability heal = Ability("Heal", "Heal all status effects", true, false, 0, 0, 1, 0, 0);
-    static Ability fireArrow = Ability("Fire Arrow", "Deal damage to one enemy and put him on fire for 2 moves", false, false, 0.5, 0, 0, 2, 0);
-    static Ability heavyArrow = Ability("Heavy Arrow", "Deal damage to one enemy and stun him for 1 move", false, false, 0.7, 0, 0, 0, 1);
+    static Ability basicAttack = Ability("Basic Attack", "Deal damage to one enemy", Drawer::getSprite("SampleAbility"), false, false, 1, 0, 0, 0, 0);
+    static Ability shieldBash = Ability("Shield Bash", "Stun one enemy for 2 moves", Drawer::getSprite("SampleAbility"), false, false, 0.5, 0, 0, 0, 2);
+    static Ability roar = Ability("Roar", "Heal one ally", Drawer::getSprite("SampleAbility"), true, false, 0, 0, 1, 0, 0);
+    static Ability fireStorm = Ability("Fire Storm", "Deal damage to all enemies", Drawer::getSprite("SampleAbility"), false, true, 0.3, 0, 0, 0, 0);
+    static Ability heal = Ability("Heal", "Heal all status effects", Drawer::getSprite("SampleAbility"), true, false, 0, 0, 1, 0, 0);
+    static Ability fireArrow = Ability("Fire Arrow", "Deal damage to one enemy and put him on fire for 2 moves", Drawer::getSprite("SampleAbility"), false, false, 0.5, 0, 0, 2, 0);
+    static Ability heavyArrow = Ability("Heavy Arrow", "Deal damage to one enemy and stun him for 1 move", Drawer::getSprite("SampleAbility"), false, false, 0.7, 0, 0, 0, 1);
 }
 
 namespace SampleHeroes {
